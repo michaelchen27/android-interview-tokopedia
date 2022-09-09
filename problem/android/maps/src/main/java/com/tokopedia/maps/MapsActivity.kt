a4a2a928e1ad80ce3f392dc1f7698756e628f8fc
Michael <michael.chen2701@gmail.com>
2022-09-09 17:53:13 +0700

    Set TextViews

--- problem/android/maps/src/main/java/com/tokopedia/maps/MapsActivity.kt
+++ problem/android/maps/src/main/java/com/tokopedia/maps/MapsActivity.kt
@@ -9,18 +9,14 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.gson.Gson
 import com.tokopedia.maps.data.api.RestCountriesInterface
-import com.tokopedia.maps.vo.Country
-import io.reactivex.android.schedulers.AndroidSchedulers
+import com.tokopedia.maps.vo.CountryItem
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
 import okhttp3.ResponseBody
 import org.json.JSONArray
 import org.json.JSONObject
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
+import retrofit2.*
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -39,6 +35,8 @@ open class MapsActivity : AppCompatActivity() {
 
     private val BASE_URL = "https://restcountries.com/"
     private var myCompositeDisposable: CompositeDisposable? = null
+
+    private val gson: Gson = Gson()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -66,6 +64,14 @@ open class MapsActivity : AppCompatActivity() {
             // search by the given country name, and
             // 1. pin point to the map
             // 2. set the country information to the textViews.
+
+            // Loading
+            textCountryName.text = ("Nama Negara: Loading...")
+            textCountryCapital.text = ("Ibukota: Loading...")
+            textCountryPopulation.text = ("Jumlah Penduduk: Loading...")
+            textCountryCallCode.text = ("Kode Telepon: Loading...")
+
+
             val searchQuery = editText?.text.toString()
             if (searchQuery.isNotEmpty()) {
                 loadData(searchQuery)
@@ -93,31 +99,50 @@ open class MapsActivity : AppCompatActivity() {
                     if (response.body() != null) {
                         val jsonResponseString = response.body()?.string()
                         Log.d(TAG, "onResponse: $jsonResponseString")
-                        setupNativeName(jsonResponseString.toString())
+                        setupTextViews(jsonResponseString.toString())
                     }
+                } else {
+                    errToast(response.code())
                 }
             }
 
+            // 200
             override fun onFailure(call: Call<ResponseBody?>, t: Throwable) {
-                TODO("Not yet implemented")
             }
         })
-
-        myCompositeDisposable?.add(
-            api.getCountryDetails(countryName)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.io())
-                .subscribe(this::handleResponse)
-        )
     }
 
-    private fun setupNativeName(responses: String) {
+    private fun errToast(code: Int) {
+        val msg = if (code == 404) "Data not found" else "Something went wrong"
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+
+        if (code == 404) {
+            textCountryName.text = ("Nama Negara:")
+            textCountryCapital.text = ("Ibukota:")
+            textCountryPopulation.text = ("Jumlah Penduduk:")
+            textCountryCallCode.text = ("Kode Telepon:")
+        }
+    }
+
+
+    private fun setupTextViews(responses: String) {
         try {
-            Log.d(TAG, "setupNativeName: $responses")
+            Log.d(TAG, "RESPONSE: $responses")
             val jsonArray = JSONArray(responses)
             val arrayLength = jsonArray.length()
             if (arrayLength > 0) {
                 val lastObj = jsonArray.getJSONObject(arrayLength - 1)
+                val json = lastObj.toString()
+                val countryItem: CountryItem = gson.fromJson(json, CountryItem::class.java)
+
+                val capital = countryItem.capital
+                val population = countryItem.population.toString()
+                val callCode = "${countryItem.idd.root}${countryItem.idd.suffixes.first()}"
+
+                textCountryCapital.text = ("Ibukota: $capital")
+                textCountryPopulation.text = ("Jumlah penduduk: $population")
+                textCountryCallCode.text = ("Kode Telepon: $callCode")
+
                 val nameObj = JSONObject(lastObj.get("name").toString())
                 val nativeNameObj = JSONObject(nameObj.get("nativeName").toString())
 
@@ -133,34 +158,16 @@ open class MapsActivity : AppCompatActivity() {
                     val firstNativeNameObj =
                         JSONObject(nativeNameObj.get(dynamicKeys[0]).toString())
                     val commonName = firstNativeNameObj.get("common")
-                    Log.d("fnm", "FIRST NATIVE NAME: $commonName")
+                    textCountryName.text = ("Nama Negara: $commonName")
                 }
             }
-
-
-        } catch (e: Exception) {
-            Log.e(TAG, "setupNativeNameError: $e")
+        } catch (e: HttpException) {
+            Log.e(TAG, "setupTextViews: ${e.message}")
+            val msg = if (e.code() == 404) "Data not found" else "Something went wrong"
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
         }
     }
 
-    // Try using GsonConverterFactory
-    private fun handleResponse(countryDetails: Country) {
-        val lastCountryDetails = countryDetails.last()
-        try {
-//            val name = lastCountryDetails.name.nativeName.toString()
-            val capital = lastCountryDetails.capital.first()
-            val population = lastCountryDetails.population.toString()
-            val callCode =
-                "${lastCountryDetails.idd.root}${lastCountryDetails.idd.suffixes.last()}"
-
-            textCountryCapital.text = ("Ibukota: $capital")
-            textCountryPopulation.text = ("Jumlah penduduk: $population")
-            textCountryCallCode.text = ("Kode Telepon: $callCode")
-        } catch (e: Exception) {
-            Log.e(TAG, "handleResponse: $e")
-            Toast.makeText(this, "${e.message}", Toast.LENGTH_SHORT).show()
-        }
-    }
 
     fun loadMap() {
         mapFragment!!.getMapAsync { googleMap -> this@MapsActivity.googleMap = googleMap }
@@ -171,3 +178,38 @@ open class MapsActivity : AppCompatActivity() {
         myCompositeDisposable?.clear()
     }
 }
+
+
+/* UNUSED CODE
+*
+*
+* //        myCompositeDisposable?.add(
+//            api.getCountryDetails(countryName)
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribeOn(Schedulers.io())
+//                .subscribe(this::handleResponse)
+//        )
+*
+*
+*
+*
+* //    // Try using GsonConverterFactory
+//    private fun handleResponse(countryDetails: Country) {
+//        try {
+//            val lastCountryDetails = countryDetails.last()
+//            val capital = lastCountryDetails.capital.first()
+//            val population = lastCountryDetails.population.toString()
+//            val callCode =
+//                "${lastCountryDetails.idd.root}${lastCountryDetails.idd.suffixes.last()}"
+//
+//            textCountryCapital.text = ("Ibukota: $capital")
+//            textCountryPopulation.text = ("Jumlah penduduk: $population")
+//            textCountryCallCode.text = ("Kode Telepon: $callCode")
+//        } catch (e: Exception) {
+//            Log.e(TAG, "handleResponse: $e")
+//            Toast.makeText(this, "${e.message}", Toast.LENGTH_SHORT).show()
+//        }
+//    }
+*
+*
+* */
